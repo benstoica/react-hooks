cdb4745e6a82f7159f7f2f6e51fc041507aece19
Ben Stoica <bstoica@uwm.com>
2022-05-24 16:27:48 -0400

    Exercise 2 - Extra Credit 3

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,21 +3,25 @@
 
 import * as React from 'react'
 
+function useLocalStorageState(key, defaultValue = '') {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) ?? defaultValue,
+  )
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state)
+  }, [key, state])
+  return [state, setState]
+}
+
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
 
-  const [name, setName] = React.useState(
-    () => window.localStorage.getItem('name') ?? initialName,
-  )
-
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
-  React.useEffect(() => {
-    window.localStorage.setItem('name', name)
-  }, [name])
 
+  const [name, setName] = useLocalStorageState('name', initialName)
   function handleChange(event) {
     setName(event.target.value)
   }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './styles.css'
 import codegen from 'codegen.macro'
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 codegen`module.exports = require('@kentcdodds/react-workshop-app/codegen')`
